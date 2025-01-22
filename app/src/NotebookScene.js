1d60c44e3c84bf299bac00c4ff75d52e56de35e1
Antonio Reid <read.musik@gmail.com>
2025-01-22 00:50:13 -0600

    notebookscene

--- app/src/NotebookScene.js
+++ app/src/NotebookScene.js
@@ -1,12 +1,19 @@
 import { useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
 
 function NotebookScene() {
     const gltf = useLoader(GLTFLoader, '/vintage-leather-book.glb');
 
     return (
-        <primitive object={gltf.scene} />
-    )
+        <>
+            <primitive object={gltf.scene} />
+            <mesh>
+                <boxGeometry />
+                <meshBasicMaterial color="red" />
+            </mesh>
+        </>
+    );
 }
 
 export default NotebookScene
